a91b5bda97ca30fae0aebaaa6d005b01d713cc70
Richard Chyan <33560310+richardchyan@users.noreply.github.com>
2021-08-07 14:49:04 -0400

    added more text

--- src/Team.js
+++ src/Team.js
@@ -12,32 +12,32 @@ const Team = () => {
             {/* portrait 1 */}
             <div className="sm:flex sm:space-x-10 justify-evenly">
                <img src={stockpor1} alt="Dr.Jones" 
-               className="mb-2 sm:w-1/3 "/>
+               className="mb-2 sm:w-1/3 object-cover"/>
                 <div className="md:w-1/2 text-left">
                   <h2 className="text-3xl uppercase">Optometrist</h2>
                   <h3 className="text-lg">Dr. Rachel Jones</h3>
-                  <p className="font-roboto mt-5">Dr. Rachel Jones grew up in Markham, Ontario. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ducimus praesentium! Ipsam similique suscipit architecto deserunt rem magnam vel. Reiciendis, praesentium? Provident accusantium quibusdam fuga error hic dolorum veniam quas reprehenderit voluptatibus. Laboriosam officia tempore et est, eveniet quos laudantium perferendis harum ad quisquam architecto corrupti, earum vero exercitationem a!</p>
+                  <p className="font-roboto mt-5">Dr. Rachel Jones grew up in Markham, Ontario. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ducimus praesentium! Ipsam similique suscipit architecto deserunt rem magnam vel. Reiciendis, praesentium? Provident accusantium quibusdam fuga error hic dolorum veniam quas reprehenderit voluptatibus. Laboriosam officia tempore et est, eveniet quos laudantium perferendis harum ad quisquam architecto corrupti, earum vero exercitationem a! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo esse quisquam suscipit necessitatibus sit, obcaecati tempore corrupti vel quasi repellendus natus nostrum ad earum a. Pariatur eveniet praesentium veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, voluptatibus.</p>
                   </div>
                </div>
               
             {/* portrait 2 */}
             <div className="sm:flex sm:space-x-10 justify-evenly">
                <img src={stockpor2} alt="Dr.Lee"
-               className="mb-2 sm:w-1/3 " />
+               className="mb-2 sm:w-1/3 object-cover" />
                <div className="md:w-1/2 text-left">
                   <h2 className="text-3xl uppercase">Optometrist</h2>
                   <h3 className="text-lg">Dr. John Lee</h3>
-                  <p className="font-roboto mt-5">Dr. Rachel Jones grew up in Toronto, Ontario. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ducimus praesentium! Ipsam similique suscipit architecto deserunt rem magnam vel. Reiciendis, praesentium? Provident accusantium quibusdam fuga error hic dolorum veniam quas reprehenderit voluptatibus. Laboriosam officia tempore et est, eveniet quos laudantium perferendis harum ad quisquam architecto corrupti, earum vero exercitationem a!</p>      
+                  <p className="font-roboto mt-5">Dr. Rachel Jones grew up in Toronto, Ontario. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ducimus praesentium! Ipsam similique suscipit architecto deserunt rem magnam vel. Reiciendis, praesentium? Provident accusantium quibusdam fuga error hic dolorum veniam quas reprehenderit voluptatibus. Laboriosam officia tempore et est, eveniet quos laudantium perferendis harum ad quisquam architecto corrupti, earum vero exercitationem a! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo esse quisquam suscipit necessitatibus sit, obcaecati tempore corrupti vel quasi repellendus natus nostrum ad earum a. Pariatur eveniet praesentium veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, voluptatibus.</p>      
                </div>
             </div>
             {/* portrait 3 */}
             <div className="sm:flex sm:space-x-10 justify-evenly">
                <img src={stockpor3} alt="Dr.Smith" 
-               className="mb-2 sm:w-1/3 "/>
+               className="mb-2 sm:w-1/3 object-cover"/>
                <div className="md:w-1/2 text-left">
                   <h2 className="text-3xl uppercase">Optometrist</h2>
                   <h3 className="text-lg">Dr. Jane Smith</h3>
-                  <p className="font-roboto mt-5">Dr. Jane Smith grew up in Markham, Ontario. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ducimus praesentium! Ipsam similique suscipit architecto deserunt rem magnam vel. Reiciendis, praesentium? Provident accusantium quibusdam fuga error hic dolorum veniam quas reprehenderit voluptatibus. Laboriosam officia tempore et est, eveniet quos laudantium perferendis harum ad quisquam architecto corrupti, earum vero exercitationem a!</p>
+                  <p className="font-roboto mt-5">Dr. Jane Smith grew up in Markham, Ontario. Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ducimus praesentium! Ipsam similique suscipit architecto deserunt rem magnam vel. Reiciendis, praesentium? Provident accusantium quibusdam fuga error hic dolorum veniam quas reprehenderit voluptatibus. Laboriosam officia tempore et est, eveniet quos laudantium perferendis harum ad quisquam architecto corrupti, earum vero exercitationem a! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo esse quisquam suscipit necessitatibus sit, obcaecati tempore corrupti vel quasi repellendus natus nostrum ad earum a. Pariatur eveniet praesentium veniam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, voluptatibus.</p>
                </div>
             </div>
          </div>
